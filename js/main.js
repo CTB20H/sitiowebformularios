ba98c21dbb65f5bfbc7b41760b4c16cf0dbcf461
CLH <matematicogoku@gmail.com>
2025-11-19 11:41:41 -0700

    Cambios del 2do: Trabajo en preparatoria, inicio en finternos e inicio de la key del firebase

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDrqpxW184Omneywk7jj2gO5yfLmCOdtgI",
+    authDomain: "sitiowebformularios.firebaseapp.com",
+    projectId: "sitiowebformularios",
+    storageBucket: "sitiowebformularios.firebasestorage.app",
+    messagingSenderId: "1092171196018",
+    appId: "1:1092171196018:web:f8e4887c2b621b2dcb04eb"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
